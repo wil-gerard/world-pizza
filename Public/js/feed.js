23009cdac84695604a356fce5c625bf80d3641de
Wolfgang-stack <wolfgang.a.mcrae@gmail.com>
2021-05-11 15:42:26 -0400

    Added new styles and restructured ejs
    Added new styling for the individual post page - changed structure slightly
    
    Changed layout of the post-footer element
    
    added styling to the upload forms on the profile page - will need to further the progress on profile pic upload form

--- Public/js/feed.js
+++ Public/js/feed.js
@@ -1,4 +1,9 @@
-function getSelectValue() {
-    let selectedValue = document.getElementById('sortBy')
-    console.log(selectedValue)
-}
+var elem = document.querySelector(".grid");
+    var msnry = new Masonry(elem, {
+        // options
+        itemSelector: ".grid-item",
+    });
+    imagesLoaded( elem ).on( 'progress', function() {
+    // layout Masonry after each image loads
+    msnry.layout();
+    });
